54837b34e7910c391d1806192d1fdeedc83ef82e
samarthKaran <40848461+samarthKaran@users.noreply.github.com>
2020-08-05 02:41:59 +0530

    Add files via upload
    Contains a small script to test a login page on a site with given credentials using chrome drivers 
    Different Drivers can further be updated for different browsers like Safari and IE

---
+++ selenuim.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 21:50:58 2020
+
+@author: samarth
+"""
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://accounts.xyz.com/login")
+
+assert "XYZ" in driver.title
+
+username = driver.find_element_by_id("Email")
+username.clear()
+username.send_keys("abc@gmail.com")
+
+password = driver.find_element_by_name("Password")
+password.clear()
+password.send_keys("pwd123#")
+
+driver.find_element_by_name("LOGIN").click()
